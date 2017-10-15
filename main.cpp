215faa9c54f07816d90fff974200c8da7a77eaa8
seanalex810 <salex@mail.csuchico.edu>
2017-10-15 02:20:43 -0700

    first draft

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(){
 Bitmap image; 
 string filename;
 vector< vector <Pixel> > bmp; 
+vector< vector <Pixel> > newImage; 
 cout<< "enter an image file"; 
 while(true){
   cin>> filename; 
@@ -15,7 +16,24 @@ while(true){
     break; 
   else 
     cout<< "enter another image"; 
+} 
+bmp = image.toPixelMatrix(); 
+Pixel rgb_old, rgb_new;
+int pixelAverage;
+newImage.resize(bmp.size());
+for (int row=0;row< bmp.size();row++){
+  newImage[row].resize(bmp[row].size());
+  for(int col=0;col< bmp[row].size(); col++){ 
+    rgb_old = bmp[row][col];
+    pixelAverage = (rgb_old.red + rgb_old.green + rgb_old.blue)/3;
+    rgb_new.red = pixelAverage; 
+    rgb_new.blue = pixelAverage; 
+    rgb_new.green = pixelAverage; 
+    newImage[row][col] = rgb_new;
+  }  
 }
+image.fromPixelMatrix(newImage);
+image.save("oldtimey.bmp"); 
 return 0 ; 
 } 
 // 
